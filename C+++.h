af1fedd3d18517c5e80028829c744dfae8650bd2
Vladimir Melnikov <81383125+Vovanm88@users.noreply.github.com>
2021-11-01 01:01:32 +0300

    The Language Itself
    Added.

---
+++ C+++.h
@@ -0,0 +1,17 @@
+#define plusplusplus -=-2
+#define List vector
+#define of <
+#define doubles double>
+#define floats float> 
+#define int64s long long>
+#define int32s int>
+#define int16s short>
+#define int8s char>
+#define ints int>
+#define uint64s unsigned long long>
+#define uint32s unsigned int>
+#define uint16s unsigned short>
+#define uint8s unsigned char>
+#define uints unsigned int>
+#define bools unsigned bool>
+
